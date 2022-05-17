c935fb03490382aee92fe4e712bdb9593b1894ab
Komil <78084162+Komil00@users.noreply.github.com>
2022-05-17 22:38:38 +0500

    Update settings.py

--- django_movies/settings.py
+++ django_movies/settings.py
@@ -125,7 +125,7 @@ LOGOUT_REDIRECT_URL = '/'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST_USER = 'komiltuev@gmail.com'
-# EMAIL_HOST_PASSWORD = '622-094-976'
+# EMAIL_HOST_PASSWORD = ''
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 #
